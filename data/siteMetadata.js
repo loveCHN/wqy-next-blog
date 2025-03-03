d62b5d6fad697170038461b3f5df87f70e546682
wangqingyun <hxcwangqingyun@163.com>
2025-03-03 16:48:34 +0800

    第二次推送

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,6 +1,6 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
+  title: '王青云的博客',
   author: 'Tails Azimuth',
   headerTitle: 'TailwindBlog',
   description: 'A blog created with Next.js and Tailwind.css',
